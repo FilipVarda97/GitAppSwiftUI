4e2e5da736ce95396b204caa56a5de836bf9c7ec
Filip Varda <filip.v@tokhit.com>
2023-03-03 22:42:03 +0100

    Initial Commit

---
+++ GitAppSwiftUI/GitAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitAppSwiftUIApp.swift
+//  GitAppSwiftUI
+//
+//  Created by Filip Varda on 03.03.2023..
+//
+
+import SwiftUI
+
+@main
+struct GitAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
